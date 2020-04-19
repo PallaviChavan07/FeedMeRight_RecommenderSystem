c779db4be198fb7337da7b02795de7ac476e8c80
Pallavi <j.pallavi07@gmail.com>
2020-04-18 19:54:51 -0700

    Normalpredictor algo

--- code/Master_Recipe_RecommenderSystem.py
+++ code/Master_Recipe_RecommenderSystem.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import code.Normal_Predictor as np
 import code.Collaborative_item_item as cii
 import code.Collaborative_user_user as cuu
 import code.ContentBased as cb
@@ -15,6 +16,7 @@ test_rating_df = pd.read_csv('../data/small_10k/core-data-test_rating.csv')
 benchmark = []
 
 #cb.ComputeContentBasedFiltering(recipe_df, train_rating_df, pd)
+np.Normalpredictor(recipe_df, train_rating_df, pd, benchmark)
 cii.ComputeCollaborativeFiltering_Item_Item(recipe_df, train_rating_df, pd, benchmark, knnmeans=False)
 cii.ComputeCollaborativeFiltering_Item_Item(recipe_df, train_rating_df, pd, benchmark, knnmeans=True)
 cuu.ComputeCollaborativeFiltering_User_User(recipe_df, train_rating_df, pd, benchmark, knnmeans=False)
@@ -26,5 +28,5 @@ coclust.Coclustering(recipe_df, train_rating_df, pd, benchmark)
 
 pd.set_option("display.max_rows", None, "display.max_columns", None)
 results = pd.DataFrame.from_records(benchmark, exclude=['MSE', 'FCP'], columns=['RMSE', 'MAE', 'MSE', 'FCP', 'PrecisionAt10', 'RecallAt10'],
-                                    index=['KNNBasic_Item_Item', 'KNNWithMeans_Item_Item', 'KNNBasic_User_User', 'KNNWithMeans_Item_Item', 'SVD', 'SVD++', 'SlopeOne', 'CoClustering'])
+                                    index=['NormalPredictor', 'KNNBasic_Item_Item', 'KNNWithMeans_Item_Item', 'KNNBasic_User_User', 'KNNWithMeans_Item_Item', 'SVD', 'SVD++', 'SlopeOne', 'CoClustering'])
 print(results)
